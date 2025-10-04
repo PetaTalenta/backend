7c1bd3fbc117850ec3ab2fa3f804b13e2394fb12
rayinailham <rayinailham9@gmail.com>
2025-10-04 12:23:46 +0700

    Convert services to git submodules

--- admin-service/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const logger = console;
-
-const PORT = process.env.PORT || 3007;
-
-app.listen(PORT, () => {
-  logger.log(`Admin Service running on port ${PORT}`);
-});
-
